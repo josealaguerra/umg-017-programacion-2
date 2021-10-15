bd8a86c527c53152c166a3231cd054b95dbf480b
Jose Guerra <josealaguerra@gmail.com>
2021-10-15 16:39:06 -0600

    Cambie nombre del paquete

--- Totito/src/main/java/com/portales/domingo/progra2/jag/totito/Boton.java
+++ Totito/src/main/java/com/portales/domingo/progra2/jag/totito/Boton.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.chinautla.sabado.sistemasoperativos.jag.totito;
+package com.portales.domingo.progra2.jag.totito;
 
 
 import java.awt.Color;
